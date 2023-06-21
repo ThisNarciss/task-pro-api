c797eb005d637514924970c9658d3c0bf40f3d12
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-06-22 00:11:28 +0300

    adds auth middleware
    plus minor improve to HttpError with message for standartization of the responses without passed messages to helper

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,3 +1,27 @@
-const authenticate = async (req, _, next) => {};
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const { HttpError } = require("../utils");
+const { SECRET_KEY } = process.env;
+
+const authenticate = asyncHandler(async (req, _, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401, "Not authorisation token"));
+  }
+  if (!token) {
+    next(HttpError(401, "Not authorisation token"));
+  }
+
+  const { id } = jwt.verify(token, SECRET_KEY);
+  console.log(id);
+  // надо сервисы юзеров, без них дальше никак
+  //   const user = await
+  //   if (!user || !user.token) {
+  // next(HttpError(401));
+  //   }
+  //   req.user = user;
+  next();
+});
 
 module.exports = authenticate;
--- utils/HttpError.js
+++ utils/HttpError.js
@@ -1,4 +1,11 @@
-const HttpError = (status, message) => {
+const defaultErrorsMessages = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not found",
+};
+
+const HttpError = (status, message = defaultErrorsMessages[status]) => {
   const error = new Error(message);
   error.status = status;
   return error;

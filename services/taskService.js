bac9786ab9f5e85fd5ce9e4a9ac714f91c68ee21
Nazarenko Alex <nazarenko.alx@gmail.com>
2023-06-22 17:13:59 +0300

    adds task services
    task services
    update for task models

---
+++ services/taskService.js
@@ -0,0 +1,54 @@
+const { Task } = require("../models");
+
+class TaskService {
+  async add(data) {
+    const result = await Task.create({ ...data });
+    if (!result) {
+      return null;
+    }
+    return result;
+  }
+
+  async delete(id) {
+    const deletedTask = await Task.findOneAndDelete({ _id: id });
+    if (!deletedTask) {
+      return null;
+    }
+    return deletedTask;
+  }
+
+  async getAll(owner) {
+    const allTasks = await Task.find({ owner });
+    if (!allTasks) {
+      return null;
+    }
+    return allTasks;
+  }
+
+  async getById(id) {
+    const task = await Task.findOne({ _id: id });
+    if (!task) {
+      return null;
+    }
+    return task;
+  }
+
+  async changeOwner(id, data) {
+    const result = await Task.findByIdAndUpdate(id, { ...data });
+    if (!result) {
+      return null;
+    }
+    return result;
+  }
+
+  async update(id, data) {
+    const result = await Task.findOneAndUpdate({ _id: id }, { ...data });
+    if (!result) {
+      return null;
+    }
+
+    return result;
+  }
+}
+
+module.exports = new TaskService();

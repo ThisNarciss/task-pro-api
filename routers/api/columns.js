69baf7b931aedda12dfb09011a63bc78e2550712
sergeyalekseev1976 <alekseev_ss@ukr.net>
2023-06-23 00:14:16 +0300

    add routers

--- routers/api/columns.js
+++ routers/api/columns.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/columns");
+const { columnJoiSchema } = require("../../models");
+const { validateBody } = require("../../utils");
 
 const { authenticate, isValidId } = require("../../middlewares");
 
-router.get("/", authenticate, ctrl.getColumns);
+router.get("/", authenticate, validateBody(columnJoiSchema), ctrl.getColumns);
 
 router.get("/:columnId", authenticate, isValidId, ctrl.getColumnById);
 
-router.post("/", authenticate, ctrl.addColumn);
+router.post("/", authenticate, validateBody(columnJoiSchema), ctrl.addColumn);
 
 router.delete("/:columnId", authenticate, isValidId, ctrl.deleteColumn);
 
-router.patch("/:columnId", authenticate, isValidId, ctrl.editColumn);
+router.patch(
+  "/:columnId",
+  authenticate,
+  isValidId,
+  validateBody(columnJoiSchema),
+  ctrl.editColumn
+);
 
 module.exports = router;
